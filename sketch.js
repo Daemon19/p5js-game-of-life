4e5eb4b0c12aa87522f1994f4bec8db723f51bf8
Daemon19 <komanglangendria04@gmail.com>
2022-06-02 14:21:59 +0800

    create grid and handling input

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+const cellSize = 20;
+
+const grid = [];
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    initGrid();
 }
 
 function draw() {
+    handleInput();
     background(40);
+    drawGrid();
+}
+
+function handleInput() {
+    if (!mouseIsPressed) return;
+
+    const row = floor(mouseY / cellSize);
+    const col = floor(mouseX / cellSize);
+    grid[row][col] = true;
+}
+
+function drawGrid() {
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            const live = grid[r][c];
+            if (!live) continue;
+
+            const x = c * cellSize;
+            const y = r * cellSize;
+            noStroke();
+            fill("limegreen");
+            rect(x, y, cellSize, cellSize);
+        }
+    }
+}
+
+function initGrid() {
+    for (let r = 0; r < windowHeight / cellSize; r++) {
+        grid.push([]);
+
+        for (let c = 0; c < windowWidth / cellSize; c++) {
+            grid[r].push(false);
+        }
+    }
 }
